7cda95635208beb90591890d4abaae8605c9bc55
DannySamy3 <danielntunduye@gmail.com>
2025-02-03 19:20:34 +0300

    fixing orentation

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -98,7 +98,7 @@ const Login = () => {
           GATE PASS SYSTEM
         </section>
 
-        <section className='w-[50%] mt-40'>
+        <section className='w-[50%] flex flex-col items-center justify-center'>
           <div className='flex justify-center text-black font-montserrat font-[500] mb-20 text-[32px]'>
             LOGIN
           </div>
