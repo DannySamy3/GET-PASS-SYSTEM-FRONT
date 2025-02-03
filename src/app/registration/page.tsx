e4b4c0a8887ff63e35b66a2fc3f68dd8dafeb755
DannySamy3 <danielntunduye@gmail.com>
2025-02-03 19:16:18 +0300

    fixing font

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -4,7 +4,7 @@ import Register from "@/components/register/Register";
 const page = () => {
   return (
     <div className=' flex h-screen '>
-      <section className=' w-[50%] font-[500] flex text-[32px] justify-center items-center'>
+      <section className=' w-[50%] text-black font-[500] flex text-[32px] justify-center items-center'>
         GATE PASS SYSTEM
       </section>
       <Register />
--- src/app/verifyToken/page.tsx
+++ src/app/verifyToken/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div className=' flex h-screen overflow-y-hidden '>
-      <section className=' w-[50%] font-[500] flex text-[32px] justify-center items-center'>
+      <section className=' w-[50%] text-black font-[500] flex text-[32px] justify-center items-center'>
         GATE PASS SYSTEM
       </section>
       <VerifyToken />
