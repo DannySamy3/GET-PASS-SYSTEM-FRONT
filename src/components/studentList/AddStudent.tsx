98a25e009e7b98976cfa830d90440029514804c0
DannySamy3 <danielntunduye@gmail.com>
2025-02-26 16:51:22 +0300

    added adding picture

--- src/components/studentList/AddStudent.tsx
+++ src/components/studentList/AddStudent.tsx
@@ -25,6 +25,13 @@ interface User {
   classId: string;
   gender: string;
   enrollmentYear: string;
+  image: File | null;
+}
+
+interface AddStudentResponse {
+  data: {
+    student: User;
+  };
 }
 
 export const AddStudent = () => {
@@ -39,17 +46,16 @@ export const AddStudent = () => {
     classId: "",
     gender: "",
     enrollmentYear: "",
+    image: null,
   });
 
   const [sponsors, setSponsors] = useState<any[]>([]);
-
   const [classes, setClasses] = useState<any[]>([]);
   const [isPageLoaded, setIsPageLoaded] = useState<boolean>(false);
-  const [isToastShown, setIsToastShown] = useState<boolean>(false); // Track toast visibility
+  const [isToastShown, setIsToastShown] = useState<boolean>(false);
 
   const dispatch = useDispatch();
 
-  // Fetch sponsors from API
   const handleFetchSponsor = async () => {
     try {
       const response = await getSponsors();
@@ -61,7 +67,6 @@ export const AddStudent = () => {
     }
   };
 
-  // Fetch classes from API
   const handleFetchClasses = async () => {
     try {
       const response = await getClasses();
@@ -73,29 +78,42 @@ export const AddStudent = () => {
     }
   };
 
-  // Handle form field change
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { value, name } = e.target;
-    setUserInfo((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    if (name === "image" && (e.target as HTMLInputElement).files) {
+      setUserInfo((prev) => ({
+        ...prev,
+        image: (e.target as HTMLInputElement).files![0],
+      }));
+    } else {
+      setUserInfo((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
   };
 
-  // Handle form submission (Add student)
   const addNewStudent = async () => {
     const currentYear = new Date().getFullYear().toString();
     userInfo.enrollmentYear = currentYear;
 
+    const formData = new FormData();
+    Object.keys(userInfo).forEach((key) => {
+      formData.append(key, userInfo[key as keyof User] as any);
+    });
+    // if (userInfo.image) {
+    //   formData.append("image", userInfo.image);
+    // }
+
     try {
-      const response = await addStudent(userInfo);
-      if (response?.data) {
+      const response = (await addStudent(formData)) as AddStudentResponse;
+      if (response.data) {
         dispatch(
           showToast({ message: "Student added successfully!", type: "success" })
         );
-        setIsToastShown(true); // Set toast as shown after successful submission
+        setIsToastShown(true);
 
         setUserInfo({
           firstName: "",
@@ -108,6 +126,7 @@ export const AddStudent = () => {
           classId: "",
           gender: "",
           enrollmentYear: "",
+          image: null,
         });
       }
     } catch (error) {
@@ -117,11 +136,9 @@ export const AddStudent = () => {
           message:
             err.response?.data?.message ||
             "Failed to add student. Please try again.",
-
           type: "error",
         })
       );
-      // setIsToastShown(false);
 
       setUserInfo({
         firstName: "",
@@ -134,149 +151,198 @@ export const AddStudent = () => {
         classId: "",
         gender: "",
         enrollmentYear: "",
-      }); // Don't show toast if there's an error
+        image: null,
+      });
     }
   };
 
   useEffect(() => {
     handleFetchSponsor();
     handleFetchClasses();
-    // handleCountries();
-    // setIsPageLoaded(true); // Set page as loaded on initial mount
   }, []);
-
-  // useEffect(() => {
-  //   // Reset the toast state when the page is loaded and toast is not shown
-  //   if (isPageLoaded && !isToastShown) {
-  //     dispatch(showToast({ message: "", type: "success" })); // Reset toast message and type
-  //   }
-  // }, [isPageLoaded, isToastShown, dispatch]);
 
   return (
     <div className='relative'>
-      <section className='my-4 bg-gradient-to-tr from-gray-100 to-gray-50 h-screen overflow-y-scroll border font-montserrat py-4 rounded-[12px] w-full'>
+      <section className='my-4 bg-gradient-to-tr bg-gray-50 h-[86vh] border font-montserrat py-4 rounded-[12px] w-full shadow-lg'>
+        <h2 className='text-3xl font-bold text-center mb-6 text-gray-500'>
+          Add New Student
+        </h2>
         <section className='flex px-12 flex-col my-3 gap-y-[10px]'>
-          <label className='text-sm text-[#414141] font-[500]'>
-            First Name
-          </label>
-          <input
-            onChange={handleChange}
-            value={userInfo.firstName}
-            required
-            name='firstName'
-            type='text'
-            placeholder="Enter student's first name"
-            className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px]'
-          />
-          <label className='text-sm text-[#414141] font-[500]'>
-            Second Name
-          </label>
-          <input
-            name='secondName'
-            value={userInfo.secondName}
-            onChange={handleChange}
-            required
-            type='text'
-            placeholder="Enter student's middle name"
-            className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px]'
-          />
-          <label className='text-sm text-[#414141] font-[500]'>SurName</label>
-          <input
-            name='lastName'
-            value={userInfo.lastName}
-            onChange={handleChange}
-            required
-            type='text'
-            placeholder="Enter student's last name"
-            className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px]'
-          />
-          <label className='text-sm text-[#414141] font-[500]'>
-            Nationality
-          </label>
-          <select
-            name='nationality'
-            value={userInfo.nationality}
-            required
-            onChange={handleChange}
-            className='select select-sm border border-[#D9CBCB] h-[34px]'
-          >
-            <option value=''>Select Country</option>
-            {fetchCountries.map((country, i) => (
-              <option key={i} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
-          <label className='text-sm text-[#414141] font-[500]'>Gender</label>
-          <select
-            name='gender'
-            value={userInfo.gender}
-            required
-            onChange={handleChange}
-            className='select select-sm border border-[#D9CBCB] h-[34px]'
-          >
-            <option value={""}>Select Gender</option>
-            <option value='Female'>Female</option>
-            <option value='Male'>Male</option>
-          </select>
-          <label className='text-sm text-[#414141] font-[500]'>
-            Phone Number
-          </label>
-          <input
-            name='phoneNumber'
-            onChange={handleChange}
-            value={userInfo.phoneNumber}
-            type='text'
-            required
-            placeholder='Enter an active mobile number'
-            className='border input text-[#414141] input-md border-[#D9CBCB] rounded-[8px] h-[34px]'
-          />
-          <label className='text-sm text-[#414141] font-[500]'>Email</label>
-          <input
-            name='email'
-            value={userInfo.email}
-            required
-            onChange={handleChange}
-            type='email'
-            placeholder='Enter an active email'
-            className='border input text-[#414141] input-md border-[#D9CBCB] rounded-[8px] h-[34px]'
-          />
-          <label className='text-sm text-[#414141] font-[500]'>Class</label>
-          <select
-            name='classId'
-            value={userInfo.classId}
-            required
-            onChange={handleChange}
-            className='select select-sm border border-[#D9CBCB] h-[34px]'
-          >
-            <option value=''>Select Class</option>
-            {classes.map((classId, i) => (
-              <option key={i} value={classId._id}>
-                {classId.name}
-              </option>
-            ))}
-          </select>
-          <label className='text-sm text-[#414141] font-[500]'>Sponsor</label>
-          <select
-            onChange={handleChange}
-            value={userInfo.sponsorId}
-            required
-            name='sponsorId'
-            className='select select-sm border border-[#D9CBCB] h-[34px]'
-          >
-            <option value=''>Select Sponsor</option>
-            {sponsors.map((sponsorId, i) => (
-              <option key={i} value={sponsorId._id}>
-                {sponsorId.name}
-              </option>
-            ))}
-          </select>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                First Name
+              </label>
+              <input
+                onChange={handleChange}
+                value={userInfo.firstName}
+                required
+                name='firstName'
+                type='text'
+                placeholder="Enter student's first name"
+                className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px] bg-white'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                Second Name
+              </label>
+              <input
+                name='secondName'
+                value={userInfo.secondName}
+                onChange={handleChange}
+                required
+                type='text'
+                placeholder="Enter student's middle name"
+                className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px] bg-white'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                SurName
+              </label>
+              <input
+                name='lastName'
+                value={userInfo.lastName}
+                onChange={handleChange}
+                required
+                type='text'
+                placeholder="Enter student's last name"
+                className='border input input-md text-[#414141] border-[#D9CBCB] rounded-[8px] h-[34px] bg-white'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                Nationality
+              </label>
+              <select
+                name='nationality'
+                value={userInfo.nationality}
+                required
+                onChange={handleChange}
+                className='select select-sm border border-[#D9CBCB] h-[34px] bg-white'
+              >
+                <option value=''>Select Country</option>
+                {fetchCountries.map((country, i) => (
+                  <option key={i} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                Gender
+              </label>
+              <select
+                name='gender'
+                value={userInfo.gender}
+                required
+                onChange={handleChange}
+                className='select select-sm border border-[#D9CBCB] h-[34px] bg-white'
+              >
+                <option value={""}>Select Gender</option>
+                <option value='Female'>Female</option>
+                <option value='Male'>Male</option>
+              </select>
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                Phone Number
+              </label>
+              <input
+                name='phoneNumber'
+                onChange={handleChange}
+                value={userInfo.phoneNumber}
+                type='text'
+                required
+                placeholder='Enter an active mobile number'
+                className='border input text-[#414141] input-md border-[#D9CBCB] rounded-[8px] h-[34px] bg-white'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>Email</label>
+              <input
+                name='email'
+                value={userInfo.email}
+                required
+                onChange={handleChange}
+                type='email'
+                placeholder='Enter an active email'
+                className='border input text-[#414141] input-md border-[#D9CBCB] rounded-[8px] h-[34px] bg-white'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>Class</label>
+              <select
+                name='classId'
+                value={userInfo.classId}
+                required
+                onChange={handleChange}
+                className='select select-sm border border-[#D9CBCB] h-[34px] bg-white'
+              >
+                <option value=''>Select Class</option>
+                {classes.map((classId, i) => (
+                  <option key={i} value={classId._id}>
+                    {classId.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='flex flex-col'>
+              <label className='text-sm text-[#414141] font-[500]'>
+                Sponsor
+              </label>
+              <select
+                onChange={handleChange}
+                value={userInfo.sponsorId}
+                required
+                name='sponsorId'
+                className='select select-sm border border-[#D9CBCB] h-[34px] bg-white'
+              >
+                <option value=''>Select Sponsor</option>
+                {sponsors.map((sponsorId, i) => (
+                  <option key={i} value={sponsorId._id}>
+                    {sponsorId.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='flex flex-col col-span-1 md:col-span-2'>
+              <label className='text-sm text-[#414141] font-[500]'>Photo</label>
+              <div className='flex items-center'>
+                <input
+                  name='image'
+                  type='file'
+                  accept='image/*'
+                  onChange={handleChange}
+                  className='hidden'
+                  id='image-upload'
+                />
+                <label
+                  htmlFor='image-upload'
+                  className='cursor-pointer border border-[#D9CBCB] rounded-[8px] h-[34px] flex items-center justify-center px-4 py-2 bg-white text-[#414141]'
+                >
+                  Upload Photo
+                </label>
+                {userInfo.image && (
+                  <div className='ml-4'>
+                    <img
+                      src={URL.createObjectURL(userInfo.image)}
+                      alt='Student Photo'
+                      className='h-[34px] w-[34px] rounded-full object-cover'
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </section>
 
-        <div className='flex my-5 justify-end w-full'>
+        <div className='flex my-5 justify-center w-full'>
           <button
             onClick={addNewStudent}
-            className='btn rounded-[8px] border bg-[#3A7563] text-white w-[15%] mx-auto hover:text-white btn-success'
+            className='btn rounded-[8px] border bg-[#3A7563] text-white w-[25%] hover:text-white btn-success'
           >
             Register
           </button>
